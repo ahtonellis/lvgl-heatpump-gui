263c6c502e82249af25a5f3943aed7834f202a8c
Ahto Nellis <ahtonellis@gmail.com>
2025-05-04 23:24:17 +0300

    First impression

--- main/src/main.c
+++ main/src/main.c
@@ -30,7 +30,7 @@ int main(int argc, char **argv)
 
     /* Loo silt */
     label = lv_label_create(btn);
-    lv_label_set_text(label, "Tere, maailm!");
+    lv_label_set_text(label, "Tere, Ahto!");
     lv_obj_center(label);
 
     while (1) {
